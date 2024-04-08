13d068fd712ea6a1cf7690c9028ddde3e3fd8aac
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-08 14:04:04 -0400

    Added NULL comditionals to file 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_node - a function that creates a binary tree node
  * @parent: pointer to the parent node of the node to create
@@ -8,12 +7,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = NULL;
 
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
+	if (parent == NULL)
+		(*node).parent = NULL;
 	(*node).parent = parent;
 	(*node).n = value;
+	(*node).left = NULL;
+	(*node).right = NULL;
 	return (node);
 }
