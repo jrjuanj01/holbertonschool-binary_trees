ce80ee43428b54f556c9964046479983c6997816
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-08 14:23:24 -0400

    Added NULL value to file 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,6 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	(*lnode).n = value;
 	(*lnode).parent = parent;
+	(*lnode).left = NULL;
 	if ((*parent).left != NULL)
 	{
 		(*lnode).left = (*parent).left;
