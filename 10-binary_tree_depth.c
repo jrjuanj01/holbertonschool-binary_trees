197bce8f8e3dfd21d6707ec1cd4a2b200e3e8db8
Juan-Mendez <jrjuanj01@gmail.com>
2024-04-09 12:12:33 -0400

    Fixed NULL conditional of file 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,6 +6,8 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	if ((*tree).parent == NULL)
 		return (0);
 	return (binary_tree_depth((*tree).parent) + 1);
